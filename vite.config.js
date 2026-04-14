f371f7c7be75020d44c3e9ae1943f9bfc3c3786c
Witzman <witzman@witzman.de>
2026-04-14 11:27:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-
 export default defineConfig({
   server: {
-    host: true,
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
     allowedHosts: true
   }
 })
